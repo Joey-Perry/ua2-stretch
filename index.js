5a1a4b17e476f0f25c9a9355d0b7bc55b97f55f6
Joey-Perry <Joeyap84@gmail.com>
2021-06-03 21:13:47 -0500

    Finished steps 2 -4

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
-const formBtn = document.querySelector('#close-form');
-const form = document.querySelector('.form');
+const grab = (identifier) => document.querySelector(identifier);
+
+const emailList = [];
+const form = grab('.form');
+const nameInput = grab('#name');
+const emailInput = grab('#email');
+const formBtn = grab('#close-form');
+const formContainer = grab('.form-container');
 
 const toggler = () => {
     form.classList.toggle('hide');
-    if(formBtn.innerText === 'X'){
-        formBtn.innerText = '+';
+    formBtn.innerText = formBtn.innerText === 'X' ? '+' : 'X';
+}
+
+const displayThankYou = () => {
+    formContainer.innerText = 'Thank you for subscribing!';
+    setTimeout(function(){
+        formContainer.remove();
+    }, 3000);
+}
+
+const validateForm = () => {
+    if ((!nameInput.value) && (!emailInput.value)){
+        nameInput.style.border = '2px solid red';
+        emailInput.style.border = '2px solid red';
+        setTimeout(function(){
+            alert('You must enter a name and an email address to subscribe!');
+        }, 0);
+    } else if (!nameInput.value){
+        nameInput.style.border = '2px solid red';
+        setTimeout(function(){
+            alert('You must enter a name to subscribe!');
+        }, 0);
+    } else if (!emailInput.value){
+        emailInput.style.border = '2px solid red';
+        setTimeout(function(){
+            alert('You must enter an email address to subscribe!');
+        }, 0);
     } else {
-        formBtn.innerText = 'X';
+        emailList.push(
+            {
+                name: nameInput.value, 
+                email: emailInput.value,
+            }
+        )
+        displayThankYou();
     }
 }
 
+const changeBorder = (element) => {
+    element.addEventListener('change', ()=>{
+        element.style.border = 'none';
+    })
+}
+
 formBtn.addEventListener('click', toggler);
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    validateForm();
+})
+
+changeBorder(nameInput);
+changeBorder(emailInput);
